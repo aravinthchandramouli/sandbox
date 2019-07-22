412d35f7f0aa1b248d842871369c7af1d6d7f79a
keerthi-pyt <keerthi@pickyourtrail.com>
2019-07-22 17:31:14 +0530

    added comments to the function regarding how it works

--- sample.js
+++ sample.js
@@ -1,4 +1,14 @@
+/**
+ * Function will remove spaces which are more than one.
+ * example:str="converts   all multiple   spaces! "
+ * split()-Splits the string into array of words. ->["converts", "", "", "all", "multiple", "", "", "spaces!", ""]
+ * filter(Boolean)-Returns an array which contains only the true values. ->["converts", "all", "multiple", "spaces!"]
+ * join()-Join the array of words using single space. ->"converts all multiplpe spaces!"
+ * 
+ */
+
 function str(mystr){
+    
  return mystr.split(' ').filter(Boolean).join(' ')
 }
 console.log(str("  hello   world  ! ."))
